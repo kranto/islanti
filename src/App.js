a3cb6493bc54cf6836a0d911e7099eb408356447
Kyösti Ranto <kranto@gmail.com>
2020-03-30 11:38:13 +0300

    more...

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Islanti!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="testgame.html"
+          // target="_blank"
         >
-          Learn React
+        Testipeli - pelitesti
         </a>
       </header>
     </div>
