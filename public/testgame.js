763527124aa5a2e291db7c896b3f39b5608f67ef
Kyösti Ranto <kranto@gmail.com>
2020-04-01 22:03:28 +0300

    removes empty open hands

--- public/testgame.js
+++ public/testgame.js
@@ -27,6 +27,8 @@ var deck = new cards.Deck({boundingElement: $("#deck")});
 deck.addCards(cards.all); 
 deck.render({immediate:true});
 
+var openHands = [];
+
 var otherHands = [];
 for (var i = 1; i <= 3; i++) {
 	var hand = new cards.Hand({faceUp:false, boundingElement: $("#hand" + i)});
@@ -42,6 +44,7 @@ var myhand = new cards.Hand({faceUp:true, y:540, minWidth: 250,
 	},
 	drop: function(card) {
 		this.addCard(card, true);
+		removeEmptyOpenHands();
 		cards.refresh();
 	},
 	boundingElement: $("#myhand"),
@@ -67,16 +70,30 @@ var newOpen = new cards.Hand({
 			},
 			drop: function(card) {
 				this.addCard(card, true);
+				removeEmptyOpenHands();
 				cards.refresh();
 			},
 			isDraggable: function(card) {
 				return true;
 			}
 		});
+		openHands.push(openHand);
 		openHand.addCard(card, true);
+		removeEmptyOpenHands();
 		cards.refresh();
 	}
 });
+
+function removeEmptyOpenHands() {
+	for (var i = 0; i < openHands.length; i++) {
+		var openHand = openHands[i];
+		if (openHand.length == 0) {
+			openHands.splice(i, 1);
+			openHand.boundingElement.remove();
+			return;
+		}
+	}
+}
 
 discardPile = new cards.Deck({faceUp:true, boundingElement: $("#pile"),
 	canDrop: function(card) {
@@ -103,6 +120,7 @@ discardPile.click(function(card){
 		setState(true, "TURN_ACTIVE");
 		myhand.addCard(card);
 		myhand.render();
+		removeEmptyOpenHands();
 		cards.refresh();
 	}
 });
