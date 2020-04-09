7d30a78b1bcc41a5fff8e34336c16c1a4559ac67
Kyösti Ranto <kyosti.ranto@eficode.com>
2020-04-09 15:33:26 +0300

    added server stub

---
+++ server/index.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('islanti'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/islanti/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log("new connection!", new Date());
+  socket.broadcast.emit('hi');
+  socket.emit('chat message', 'Welcome!');
+  socket.on('chat message', function(msg){
+    // io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+});
+
+http.listen(4000, function(){
+  console.log('listening on *:4000');
+});
+
+
