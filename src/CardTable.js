32e3751ad3949d6b8378b02754de2f7becc693b4
Kyösti Ranto <kranto@gmail.com>
2020-04-11 08:20:14 +0300

    load full state at (re)connect

--- src/CardTable.js
+++ src/CardTable.js
@@ -29,6 +29,26 @@ class CardTable extends Component {
       game.stateChange(args);
     });
 
+    this.socket.on('connect', () => {
+      console.log('connected');
+      this.loadFullState();
+    });
+
+    this.socket.on('reconnect', () => {
+      console.log('connected');
+      this.loadFullState();
+    });
+
+    this.socket.on('reconnect_attempt', (i) => {
+      console.log('attempting to reconnect', i);
+    });
+
+    this.socket.on('disconnect', () => {
+      console.log('disconnected');
+    });
+  }
+
+  loadFullState() {
     let hash = window.location.hash;
     let id = hash && hash.length > 2 ? hash[2] : "0";
     this.socket.emit('authenticate', id);
