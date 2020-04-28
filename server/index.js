2cf417022acacc72f611d182e0b735c070b28219
Kyösti Ranto <kranto@gmail.com>
2020-04-28 08:21:20 +0300

    first docker version

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-var server = require('./serverstate');
 var lobbys = require('./Lobby');
 const storage = require('./storage');
 
@@ -15,19 +14,12 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/islanti/index.html');
 });
 
-const myArgs = process.argv.slice(2);
-// const roundNumber = parseInt(myArgs[0]);
-// const dealerIndex = parseInt(myArgs[1]);
-// const s = new server.ServerState(io, "dev");
-
 const lobby = new lobbys.Lobby(io);
 
 (async () => {
   try {
-    await storage.init();
-    console.log("initialized");
+    await storage.init(process.env.MONGO_ADDRESS);
     http.listen(4000, () => console.log('listening on *:4000'));
-    // await s.init(roundNumber, dealerIndex);
     await lobby.init();
   } catch (e) {
     console.log(e);
